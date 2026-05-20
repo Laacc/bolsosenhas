52dcb3c0aa13cc3957b06bdc8397fb43a75c3902
Hudi <leitetunado2@outlook.com>
2026-05-19 23:01:50 -0300

    refatorada leve, delete add
    Leve refatoração no trocar senhas e adicionado deletar senhas; também foram adicionados ícones nas validações.

--- main.py
+++ main.py
@@ -32,13 +32,31 @@ def consultar_senhas(caminho_arquivo):
 
 def trocar_senha(caminho_arquivo, plataforma):
     senhas = carregar_senhas(caminho_arquivo)
+    encontrado = False
     for linha in senhas:
         if linha[0] == plataforma:
+            encontrado = True
             nova_senha = gerar_senha()
             linha[1] = nova_senha
+            break
     with open(caminho_arquivo, "w", encoding="utf-8") as arquivo:
         for linha in senhas:
             arquivo.write(f"{linha[0]};{linha[1]}\n")
+    return encontrado
+
+def deletar_senha(caminho_arquivo, plataforma):
+    senhas = carregar_senhas(caminho_arquivo)
+    encontrado = False
+    for linha in senhas:
+        if linha[0] == plataforma:
+            encontrado = True
+            senhas.remove(linha)
+            break
+    with open(caminho_arquivo, "w", encoding="utf-8") as arquivo:
+        for linha in senhas:
+            arquivo.write(f"{linha[0]};{linha[1]}\n")
+    return encontrado
+
 
     
 def gerar_salvar_s():
@@ -47,10 +65,10 @@ def gerar_salvar_s():
     if plataforma != None and plataforma != "":
         senha = gerar_senha()
         salvar_senha(plataforma, senha, arquivo)
-        CTkMessagebox(title="Completo!", message="Senha gerada!")
+        CTkMessagebox(title="Completo!", message="Senha gerada!", icon="check")
     else:
         if plataforma == "":
-            CTkMessagebox(title="Operação inválida", message="Informe uma plataforma!")
+            CTkMessagebox(title="Operação inválida", message="Informe uma plataforma!", icon="cancel")
             return   
         CTkMessagebox(title="Operação cancelada", message="A Operação foi cancelada!")
         return 
@@ -62,24 +80,46 @@ def trocar():
     dialogo = CTkInputDialog(title="Alterar senha", text="Plataforma a alterar: ")
     plataforma = dialogo.get_input()
     if plataforma != None and plataforma != "":
-        trocar_senha(caminho_arquivo=arquivo, plataforma=plataforma)
-        CTkMessagebox(title="Completo!", message="Senha alterada!")
+        alterada = trocar_senha(caminho_arquivo=arquivo, plataforma=plataforma)
+        if alterada == True:
+            CTkMessagebox(title="Sucesso!", message="Senha alterada!", icon="check")
+        else:
+            CTkMessagebox(title="Erro", message="Plataforma não encontrada!", icon="cancel")
     else:
         if plataforma == "":
-            CTkMessagebox(title="Operação inválida", message="Informe uma plataforma!")
+            CTkMessagebox(title="Operação inválida", message="Informe uma plataforma!", icon="cancel")
             return
         CTkMessagebox(title="Operação cancelada", message="A Operação foi cancelada")
+        return
+    
+def deletar():
+    dialogo = CTkInputDialog(title="Deletar senha", text="Plataforma para deletar: ")
+    plataforma = dialogo.get_input()
+    if plataforma != None and plataforma != "":
+        deletada = deletar_senha(caminho_arquivo=arquivo, plataforma=plataforma)
+        if deletada == True:
+            CTkMessagebox(title="Sucesso!", message="Senha deletada!", icon="check")
+        else:
+            CTkMessagebox(title="Erro", message="Plataforma não encontrada!", icon="cancel")
+    else:
+        if plataforma == "":
+            CTkMessagebox(title="Operação inválida", message="Informe uma plataforma!", icon="cancel")
+            return
+        CTkMessagebox(title="Operação cancelada", message="A Operação foi cancelada!")
         return
 
 janela = CTk()
 janela.title("Bolsosenhas (Flávio Edition)")
 janela.geometry("500x400")
-botao1 = CTkButton(janela, text="Gerar e salvar senha", command=gerar_salvar_s, corner_radius=32, fg_color="#1C24C5", hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
+botao1 = CTkButton(janela, text="Gerar e salvar senha", command=gerar_salvar_s, corner_radius=32, hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
 botao1.place(relx=0.5, rely=0.3, anchor="center")
-botao2 = CTkButton(janela, text="Consultar senhas", command=consultar, corner_radius=32, fg_color="#1C24C5", hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
+botao2 = CTkButton(janela, text="Consultar senhas", command=consultar, corner_radius=32, hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
 botao2.place(relx= 0.5, rely=0.4, anchor="center")
-botao3 = CTkButton(janela, text="Alterar senha", command=trocar, corner_radius=32, fg_color="#1C24C5", hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
+botao3 = CTkButton(janela, text="Alterar senha", command=trocar, corner_radius=32, hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
 botao3.place(relx=0.5, rely=0.5, anchor="center")
-botao4 = CTkButton(janela, text="Sair", command=janela.destroy, corner_radius=32, fg_color="#1C24C5", hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
-botao4.place(relx=0.5, rely=0.6, anchor="center" )
+botao4 = CTkButton(janela, text="Deletar senha", command=deletar, corner_radius=32, hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
+botao4.place(relx=0.5, rely=0.6, anchor="center")
+botao5 = CTkButton(janela, text="Sair", command=janela.destroy, corner_radius=32, hover_color="#70BEEB", border_width=1, border_color="#2B4A5B")
+botao5.place(relx=0.5, rely=0.7, anchor="center" )
+
 janela.mainloop()
